a5382eec0e7531a71b982c33908603c8135b720d
Pankratov <pnrf@yandex.ru>
2022-08-02 21:17:51 +0300

    feat: add controller for login

--- controllers/users.js
+++ controllers/users.js
@@ -49,6 +49,29 @@ module.exports.createUser = (req, res) => {
     });
 };
 
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неверно указаны почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error('Неверно указаны почта или пароль'));
+      }
+
+      return res.status(httpStatusCodes.SUCСESSFUL_REQUEST).send({ message: 'Регистрация прошла успешно!' });
+    })
+    .catch((err) => {
+      res.status(httpStatusCodes.UNAUTHORIZED).send({ message: err.message });
+    });
+};
+
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
 
--- utils/httpStatusCodes.js
+++ utils/httpStatusCodes.js
@@ -1,6 +1,7 @@
 module.exports = {
   SUCСESSFUL_REQUEST: 200,
   BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
   NOT_FOUND: 404,
   SERVER_ERROR: 500,
 };
