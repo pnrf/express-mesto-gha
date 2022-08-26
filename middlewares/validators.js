ec82a3c9e2e60058589cbb918db657bd0246a852
Pankratov <pnrf@yandex.ru>
2022-08-26 15:32:44 +0300

    fix: change regex in validators

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -10,14 +10,7 @@ const validateUrl = (url) => {
   throw new BadRequestError('Невалидный URL');
 };
 
-const isRegex = (avatar) => {
-  // eslint-disable-next-line no-useless-escape
-  const regex = '/[-a-zA-Z0-9@:%_\+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/gi';
-  if (regex) {
-    return avatar;
-  }
-  throw new Error('Невалидный URL, не соответствует regex');
-};
+const urlRegex = /[-a-zA-Z0-9@:%_\+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/gi;
 
 const validateUserId = celebrate({
   params: Joi.object().keys({
@@ -29,43 +22,43 @@ const validateSignUp = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().custom(isRegex),
+    avatar: Joi.string().pattern(urlRegex),
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
+    password: Joi.string().required(),
+  }).unknown(true),
 });
 
 const validateSignIn = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-  }),
+  }).unknown(true),
 });
 
 const validateUpdateProfile = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-  }),
+  }).unknown(true),
 });
 
 const validateUpdateAvatar = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().custom(isRegex),
-  }),
+    avatar: Joi.string().pattern(urlRegex),
+  }).unknown(true),
 });
 
 const validateCardCreation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().custom(validateUrl),
-  }),
+  }).unknown(true),
 });
 
 const validateCardId = celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().hex().length(24),
-  }),
+  }).unknown(true),
 });
 
 module.exports = {
