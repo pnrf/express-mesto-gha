67d77e7535384b65d1bd64b220439a4a4c78d649
Pankratov <pnrf@yandex.ru>
2022-08-26 17:58:22 +0300

    fix: add cookie-parser, helmet to app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 const routesUsers = require('./routes/users');
 const routesCards = require('./routes/cards');
 const NotFoundError = require('./errors/not-found-error');
@@ -13,6 +15,8 @@ const errorHandler = require('./middlewares/errorHandler');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(helmet());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -31,7 +35,7 @@ app.use(errors());
 app.use(errorHandler);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
-  // useNewUrlParser: true,
+  useNewUrlParser: true,
   // useCreateIndex: true,
   // useFindAndModify: false,
 });
