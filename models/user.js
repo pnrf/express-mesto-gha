54deeed43e96cf9831b04fa514fe648551195dd8
Pankratov <pnrf@yandex.ru>
2022-08-26 11:56:40 +0300

    fix: clear code in models

--- models/user.js
+++ models/user.js
@@ -6,21 +6,21 @@ const AuthError = require('../errors/auth-error');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: false,
+    // required: false,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: false,
+    // required: false,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: false,
+    // required: false,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (url) => validator.isUrl(url),
@@ -49,13 +49,13 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new AuthError('Неверно указаны почта или пароль'));
+        throw new AuthError('Неверно указаны почта или пароль');
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new AuthError('Неверно указаны почта или пароль'));
+            throw new AuthError('Неверно указаны почта или пароль');
           }
 
           return user;
