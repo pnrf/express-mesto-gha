9d634c9c6895d32ee9c7c929cd9d078e58a0c11a
Pankratov <pnrf@yandex.ru>
2022-08-26 20:54:04 +0300

    fix: add cookies.jwt to auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,13 +3,20 @@ const AuthError = require('../errors/auth-error');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  if (!req.cookies.jwt) {
     throw new AuthError('Требуется авторизация');
   }
 
-  const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
+
+  // const { authorization } = req.headers;
+
+  // if (!authorization || !authorization.startsWith('Bearer ')) {
+  //   throw new AuthError('Требуется авторизация');
+  // }
+
+  // const token = authorization.replace('Bearer ', '');
+
   let payload;
 
   try {
